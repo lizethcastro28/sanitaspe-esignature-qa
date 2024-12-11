9f82ba9cdfc2309773fb58aede60db44fa88aa31
Lizeth Castro <lcastro@MacBook-Air-de-Lizeth.local>
2024-12-11 08:59:34 -0400

    url secret

--- amplify/functions/circuit-function/resource.ts
+++ amplify/functions/circuit-function/resource.ts
@@ -10,6 +10,6 @@ export const circuitFunction = defineFunction({
     environment: {
         OAUTH_TOKEN_URL: process.env.OAUTH_TOKEN_URL || "https://integrationlayer.auth.us-east-2.amazoncognito.com/oauth2/token",
         PROCESS_CIRCUIT_URL: process.env.PROCESS_CIRCUIT_URL || "http://biometric.integrationlayer.com/api/v1/biometric/internal/process_circuit/{circuit}",
-        SECRET_NAME: process.env.SECRET_NAME || "oauth/api"
+        SECRET_NAME: process.env.SECRET_NAME || "qa/oauth/api"
     },
 });
--- amplify/functions/config-function/resource.ts
+++ amplify/functions/config-function/resource.ts
@@ -10,6 +10,6 @@ export const configFunction = defineFunction({
     environment: {
         OAUTH_TOKEN_URL: process.env.OAUTH_TOKEN_URL || "https://integrationlayer.auth.us-east-2.amazoncognito.com/oauth2/token",
         GET_CHANEL_URL: process.env.GET_CHANEL_URL || "http://biometric.integrationlayer.com/api/v1/biometric/internal/get_channel/{circuit}",
-        SECRET_NAME: process.env.SECRET_NAME || "oauth/api"
+        SECRET_NAME: process.env.SECRET_NAME || "qa/oauth/api"
     },
 });
