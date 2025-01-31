163824dddd176bdb0c70f13260037a1575e1ae9a
Lizeth Castro <lcastro@162.0.16.172.net-uno.net>
2025-01-31 18:01:15 -0400

    prueba channel

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -219,11 +219,11 @@ const Body: React.FC<BodyProps> = ({ instructions, instructions_location }) => {
                                 console.log("----------circuit: ", circuit)
                                 console.log("----------DATA: ", data)
                                 console.log("----------circuitdata: ", circuitData)
-                                //let redirect = ""
-                                /*if (circuitData && circuitData.urlRedirect) {
+                                let redirect = ""
+                                if (circuitData && circuitData.urlRedirect) {
                                     redirect = circuitData.urlRedirect;
-                                }*/
-                                //window.location.href = redirect;
+                                }
+                                window.location.href = redirect;
                             }, 13000);
                         } else {
                             console.log('---is not live: ', data.Confidence);
