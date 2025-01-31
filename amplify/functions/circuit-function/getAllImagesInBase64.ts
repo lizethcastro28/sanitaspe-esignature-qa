8bd97aed3f7855edd460a3f1618dc9add08557ad
Lizeth Castro <lcastro@162.0.16.172.net-uno.net>
2025-01-31 14:57:46 -0400

    prueba

--- amplify/functions/circuit-function/getAllImagesInBase64.ts
+++ amplify/functions/circuit-function/getAllImagesInBase64.ts
@@ -23,12 +23,13 @@ const s3 = new S3();
  */
 export const getAllImagesInBase64 = async (session_id: string, bucket_name: string): Promise<ImageObject[]> => {
     try {
-        console.log('Bucket: ', bucket_name)
-        console.log("session: ", session_id)
+        console.log('---Bucket: ', bucket_name)
+        console.log("---session: ", session_id)
         const listObjects = await s3.listObjectsV2({
             Bucket: bucket_name,
             Prefix: session_id,
         }).promise();
+        console.log("----listObjects: ", listObjects)
         
         // Filtrar los archivos para asegurar que no incluya carpetas ni archivos vacíos
         const imageKeys = listObjects.Contents?.filter(file => file.Key && (file.Size ?? 0) > 0).map(file => file.Key) || [];
@@ -41,6 +42,7 @@ export const getAllImagesInBase64 = async (session_id: string, bucket_name: stri
             }).promise();
 
             const base64Content = object.Body ? object.Body.toString('base64') : '';
+            console.log("----base64: ", base64Content);
 
             const r = {
                 name: key!.split('/').pop()!, // Obtener solo el nombre del archivo del key
